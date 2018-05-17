99cd94c4ac831db54260db08fbbe679a4707cdb8
wulinman <550832921@qq.com>
2018-05-18 00:27:12 +0800

    add our own model & transformation
    Add our own fbx model: ten.fbx to game scene. Add transformation to the model: rotate. (without white line bug version) Use ground plane.

---
+++ Assets/Script/GermMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GermMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //float translation = Time.deltaTime * 10;
+
+        //移动的方向
+        //transform.Translate(translation, 0, 0);
+        //transform.Translate(0, translation, 0);
+        //transform.Translate(0, 0, 0.001);
+
+        // rotate
+        transform.Rotate(2, 0, 0);
+
+        //以模型Y轴旋转，单位为2.
+        transform.Rotate(0, 2, 0);
+
+        //以模型Z轴旋转，单位为2.
+        //transform.Rotate(0, 0, 2);
+    }
+}
---
+++ Assets/Script/Scene_Button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Scene_Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnClick()
+    {
+        Button button = gameObject.GetComponent<Button>() as Button;
+        if (button.name == "Rule_Button")
+        {
+            SceneManager.LoadScene("RuleScene");
+        }
+        else if (button.name == "Begin_Button")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+        else if (button.name == "Rank_Button")
+        {
+            SceneManager.LoadScene("RankScene");
+        }
+        else if (button.name == "Round_Button")
+        {
+            SceneManager.LoadScene("RoundScene");
+        } 
+        else if (button.name == "Back_Button")
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
